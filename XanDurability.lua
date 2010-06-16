18c56791f45e8c6f24520d6faf9464c7a848c95a
Xruptor <xruptor@manaflux.com>
2010-06-16 09:24:04 -0400

    -Another freaking attempt at fixing the bag durability (another typo).

--- XanDurability.lua
+++ XanDurability.lua
@@ -177,7 +177,7 @@ function f:GetDurabilityInfo()
 	for bag = 0, 4 do
 		for slot = 1, GetContainerNumSlots(bag) do
 			local hasCooldown, repairCost = tmpTip:SetBagItem(bag, slot)
-			local Minimum, Maximum = GetContainerItemDurability(slot)
+			local Minimum, Maximum = GetContainerItemDurability(bag, slot)
 
 			if repairCost and repairCost > 0 then
 				bagCost = bagCost + repairCost
